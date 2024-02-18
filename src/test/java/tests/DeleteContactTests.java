87afee52a62529600270ccc149d7048aad4d536a
LizaKenigzon <elizaveta.murashkina@bk.ru>
2024-02-18 20:52:30 +0200

    14.02.2024

---
+++ src/test/java/tests/DeleteContactTests.java
@@ -0,0 +1,34 @@
+package tests;
+
+import config.AppiumConfig;
+import models.Auth;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import screens.AuthenticationScreen;
+import screens.ContactListScreen;
+
+public class DeleteContactTests extends AppiumConfig {
+
+    @BeforeClass
+    public void preCondition(){
+        new AuthenticationScreen(driver)
+                .fillLoginRegistrationForm(Auth.builder().email("noa@gmail.com").password("Nnoa12345$")
+                        .build())
+                .submitLogin();
+    }
+
+    @Test
+    public void deleteFirstContact(){
+        new ContactListScreen(driver)
+                .deleteFirstContact()
+                .isListSizeLessOnOne();
+    }
+
+    @Test
+    public void removeAllContactSuccess(){
+        new ContactListScreen(driver)
+                .removeAllContacts()
+                .isNoContactsHere();
+
+    }
+}
